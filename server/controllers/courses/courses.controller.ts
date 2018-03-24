48e38972af43f699aaacd0fc7ce1be4fd9134fed
Nate Cheng <qqnc@users.noreply.github.com>
2018-03-23 17:53:55 -0700

    deploy

--- server/controllers/courses/courses.controller.ts
+++ server/controllers/courses/courses.controller.ts
@@ -5,9 +5,6 @@ import * as express from 'express';
 const courses_router = express.Router();
 const Course = require('../../models/courses');
 
-import * as admin from 'firebase-admin';
-const serviceAccount = require('./striped-reserve-853-firebase-adminsdk-rsa37-eba2a0f172.json');
-
 courses_router.get('/', function (req, res) {
   console.log('get courses');
   let courses = [];
