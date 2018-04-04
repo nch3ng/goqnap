dac10c4cf08b748db743d10402a166e11c3935c1
Nate Cheng <qqnc@users.noreply.github.com>
2018-04-03 17:48:34 -0700

    add limit

--- server/controllers/courses/courses.controller.ts
+++ server/controllers/courses/courses.controller.ts
@@ -13,20 +13,29 @@ courses_router.get('/', function (req, res) {
   // console.log(req.query['orderBy']);
   let orderBy;
   let desc = false;
+  let limit = 0;
   if (req.query['orderBy']) {
     orderBy = req.query['orderBy'].split(':')[0];
-
     if (req.query['orderBy'].split(':')[1] && req.query['orderBy'].split(':')[1] === 'desc') {
       desc = true;
     }
     // console.log(req.query['orderBy'].split(':')[1]);
+  }
+
+  if (req.query['limit']) {
+    limit = +req.query['limit'];
   }
   // console.log(orderBy);
   let courses = [];
 
   let sort;
   desc === true ? sort = '-' + orderBy : sort = orderBy;
-  const promise = Course.find({}).sort(sort).exec();
+  let promise;
+  if (limit === 0) {
+    promise = Course.find({}).sort(sort).exec();
+  } else {
+    promise = Course.find({}).sort(sort).limit(limit).exec();
+  }
 
   promise.then(
     (rcourses) => {
