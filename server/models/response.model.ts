51ac5e90f3b7404e445def45ea61d4d1dd37d4ef
Nate Cheng <boo0330@gmail.com>
2018-05-23 20:33:47 -0700

    refactor

--- server/models/response.model.ts
+++ server/models/response.model.ts
@@ -1,14 +1,8 @@
+import { UserCreationResponse } from './user.model';
 export interface GeneralResponse {
   success: boolean;
   message: string;
 }
 
-export class ErrorResponse implements GeneralResponse {
-  success: boolean;
-  message: string;
-
-  constructor(success: boolean, message: string) {
-    this.success = success;
-    this.message = message;
-  }
+export class ErrorResponse extends UserCreationResponse {
 }
