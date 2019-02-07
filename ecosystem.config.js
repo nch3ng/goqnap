55c28458f99d1489a992d1ea0d9f66fe8a42a08a
Nate Cheng <boo0330@gmail.com>
2019-02-07 11:33:43 -0800

    add staging config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
       winkey: '/c/Users/nate/.ssh/google_cloud_deploy_openSSH',
       key: '~/.ssh/id_rsa_deploy_google_cloud',
       user: 'deploy',
-      host: ['go.qnap.como'],
+      host: ['go.qnap.com'],
       ref: 'origin/master',
       repo: 'git@github.com:qqnc/goqnap.git',
       path: '/var/www/goqnap/staging',
