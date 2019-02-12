6beeccdab147c17770cb30458dd93920447e9564
Nate Cheng <boo0330@gmail.com>
2019-02-12 09:49:23 -0800

    change key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,8 +31,8 @@ module.exports = {
       'post-deploy' : 'cp ~/environment/goqnap/.env ./; cp ./server/helpers/email*.html dist/helpers/;sudo pm2 restart ecosystem.config.js --env production;sudo cp ../../../qnapusa/public/ . -a; sudo chown -R deploy:deploy node_modules',
     },
     staging: {
-      winkey: '/c/Users/nate/.ssh/google_cloud_deploy_openSSH',
-      key: '~/.ssh/id_rsa',
+      key: '/c/Users/nate/.ssh/google_cloud_deploy_openSSH',
+      winkey: '~/.ssh/id_rsa',
       user: 'deploy',
       host: ['68.4.6.13', 'staging-go.natecheng.me', '192.168.86.25'],
       ref: 'origin/master',
