efee0a7871770c9ba5e0862978649c5d6ab539c3
Nate Cheng <boo0330@gmail.com>
2019-12-05 12:16:22 -0800

    variable

--- server/global.available.ts
+++ server/global.available.ts
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'testing') {
   password = process.env.DB_PASSWORD;
   address = process.env.DB_ADDRESS;
   // extra = '?authSource=admin';
-  // extra = '';
+  extra = '';
 }
 
 export const global = {
